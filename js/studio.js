94f40992954de190b4ac77a8ed74092879808afd
sergkrasivskyi <sergii.krasivskyi@gmail.com>
2022-06-11 21:22:26 +0300

    going to finish

--- js/studio.js
+++ js/studio.js
@@ -1,13 +1,9 @@
+let COLOR_CURRENT = "#2196F3";
 if (window.location.pathname == "/index.html") {
-    const currentStudio = document.querySelector(".current-studio");
-    //   current.style.backgroundColor = "blue";
-      currentStudio.style.color = "blue";
-      currentStudio.style.fontSize = '40px'
-    //   text.style.fontSize = '40px'
-    }
-    if (window.location.pathname == "/portpholio.html") {
-        const currentPortpholio = document.querySelector(".current-portpholio");
-        // text.style.backgroundColor = "green";
-        currentPortpholio.style.color = "green";
-        currentPortpholio.style.fontSize = '40px'
-      }
+  const currentStudio = document.querySelector(".current-studio");
+  currentStudio.style.color = COLOR_CURRENT;
+}
+if (window.location.pathname == "/portpholio.html") {
+  const currentPortpholio = document.querySelector(".current-portpholio");
+  currentPortpholio.style.color = COLOR_CURRENT;
+}
